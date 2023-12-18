91894d1a235d176a8567b26fd6fcb410ab596e20
DewewRHINO <jrjayme@cpp.edu>
2023-12-18 12:00:23 -0800

    Added blog and about page

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
     return render_template('home.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/blog")
+def blog_page():
+    return render_template('blog.html')
